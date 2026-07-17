3fc8cf1aca5664e1b4889ba8fc30947c798ccb1c
Eda Gayathri <edagayathri2005@gmail.com>
2026-07-17 11:36:59 +0530

    Completed testing and logging frameworks assignments

---
+++ Digital_Nurture_Deepskilling/Mandatory_Exercises/Testing_and_Logging_Frameworks/testing_framework_assignments/src/main/java/com/gayathri/ExternalApi.java
@@ -0,0 +1,5 @@
+package com.gayathri;
+
+public interface ExternalApi {
+    String getData();
+}
---
+++ Digital_Nurture_Deepskilling/Mandatory_Exercises/Testing_and_Logging_Frameworks/testing_framework_assignments/src/main/java/com/gayathri/MyService.java
@@ -0,0 +1,10 @@
+package com.gayathri;
+public class MyService{
+    private ExternalApi api;
+    public MyService(ExternalApi api){
+        this.api=api;
+    }
+    public String fetchData(){
+        return api.getData();
+    }
+}
